dd5f7f56cac83c20c7959507733cd03d5cbcfe7e
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-11-14 09:02:29 +0100

    indexes for users not present in kaggle_users map

--- import_triplets_to_mongodb.py
+++ import_triplets_to_mongodb.py
@@ -11,11 +11,14 @@ TEST_TRIPLETS_FILE_PATH = 'resources/private/kaggle_visible_evaluation_triplets.
 users_map = {}
 songs_map = {}
 
+# Python closure hack
+last_index = [0]
 
 def load_kaggle_users_mapping():
     with open(KAGGLE_USERS_MAPPING_FILE_PATH) as kaggle_users:
         for index, user_id in enumerate(kaggle_users):
             users_map[user_id.strip()] = index + 1
+            last_index[0] += 1
 
 
 def load_kaggle_songs_mapping():
@@ -43,8 +46,15 @@ def import_triplets_from_file(filename, db_collection):
 
 def get_triplet_from_fileline(line):
     parts = line.strip().split()
+
+    if parts[0] in users_map:
+        user_index = users_map[parts[0]]
+    else:
+        last_index[0] += 1
+        user_index = last_index[0]
+
     return {
-        'user_index': users_map[parts[0]],
+        'user_index': user_index,
         'song_index': songs_map[parts[1]],
         'play_count': int(parts[2])
     }
@@ -56,7 +66,7 @@ invoke_measurable_task(load_kaggle_songs_mapping, 'Load Kaggle songs mapping')
 with MongoClient('localhost', MONGODB_PORT) as client:
     db = client.local
     invoke_measurable_task(
-        lambda: import_triplets_from_file(TRAIN_TRIPLETS_FILE_PATH, db.test_triplets),
+        lambda: import_triplets_from_file(TRAIN_TRIPLETS_FILE_PATH, db.train_triplets),
         'Import train triplets')
     invoke_measurable_task(
         lambda: import_triplets_from_file(TEST_TRIPLETS_FILE_PATH, db.test_triplets),
