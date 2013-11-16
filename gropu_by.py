32423f7125ee507a1a565dca87825de403262697
Taldir <mikolaj.jankowski@gmail.com>
2013-11-16 14:21:58 +0100

    Group by tests
    - Added inserting 100 most popular songs to DB (most_popular.py)
    - Added tries of group bys. (Not working)

---
+++ gropu_by.py
@@ -0,0 +1,30 @@
+from pymongo import *
+from measurements import invoke_measurable_task
+
+MONGODB_PORT = 27017
+
+def one_by_one():
+    songs = db.most_popular.find(),
+    print '100 most popular songs selected'
+    i = 0
+    
+    while(i < 100):
+        key = [ 'user_index' ]
+        condition = { 'song_index': songs[0][i]['_id'] }
+        initial = { 'count': 0, 'sum': 0 }
+        reduce = 'function(doc, out) {  }'
+        songsGroup = db.train_triplets.group(key, condition, initial, reduce)
+
+        print 'Groupby song: ',songs[0][i]
+        print 'Groupby song _id: ',songs[0][i]['_id']
+        print 'Group: ',songsGroup
+        print 'Group length: ',len(songsGroup)
+        print i+1,' percents'
+
+        i += 1
+
+with MongoClient('localhost', MONGODB_PORT) as client:
+    db = client.local
+    invoke_measurable_task(
+        lambda: one_by_one(),
+        'Group by most popular song one by one')
---
+++ most_popular.py
@@ -0,0 +1,16 @@
+from pymongo import *
+from measurements import invoke_measurable_task
+
+MONGODB_PORT = 27017
+
+def hundred_songs():
+    songs = db.play_count_by_song.find().sort('value', DESCENDING).limit(100)
+    db.most_popular.insert(songs, w=0,
+                                     j=False, fsync=False,
+                                     continue_on_error=True)
+									 
+with MongoClient('localhost', MONGODB_PORT) as client:
+    db = client.local
+    invoke_measurable_task(
+        lambda: hundred_songs(),
+        "Find 100 most often played songs")
