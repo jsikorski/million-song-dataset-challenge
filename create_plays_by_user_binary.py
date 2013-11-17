7807ed7dc797e8bd230181cfce3dc664a3be1dad
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-11-17 04:10:29 +0100

    creating binary representation for plays by user (using 100 most popular songs)

---
+++ create_plays_by_user_binary.py
@@ -0,0 +1,45 @@
+from pymongo import MongoClient, DESCENDING
+from utils.database_inserter import insert_batch
+from utils.measurements import invoke_measurable_task
+
+MONGODB_PORT = 27017
+NUMBER_OF_MOST_POPULAR_SONGS = 100
+BATCH_SIZE = 2500
+
+
+def create_plays_by_user_binary_collection(db, most_popular_songs):
+    plays_batch = []
+
+    for play_map in db.test_plays_by_user.find():
+        plays_for_user = {'_id': play_map['_id'], 'plays': {}}
+        for song in most_popular_songs:
+            song_index = str(int(song['_id']))
+            plays_for_user['plays'][song_index] = song_index in play_map['value']
+
+        plays_batch.append(plays_for_user)
+
+        if len(plays_batch) % BATCH_SIZE == 0:
+            insert_batch(plays_batch, db.train_plays_by_user_binary)
+            plays_batch = []
+
+    if len(plays_batch) > 0:
+        insert_batch(plays_batch, db.train_plays_by_user_binary)
+
+
+with MongoClient('localhost', MONGODB_PORT) as client:
+    db = client.local
+
+    most_popular_songs = [1]
+
+    def load_most_popular_songs():
+        most_popular_songs[0] = list(
+            db.train_play_count_by_song.find()
+            .sort('value', DESCENDING)
+            .limit(NUMBER_OF_MOST_POPULAR_SONGS)
+        )
+
+    invoke_measurable_task(load_most_popular_songs, 'Load %d most popular songs' % NUMBER_OF_MOST_POPULAR_SONGS)
+    most_popular_songs = most_popular_songs[0]
+
+    invoke_measurable_task(lambda: create_plays_by_user_binary_collection(db, most_popular_songs),
+                           "Create plays_by_user_binary collection")
