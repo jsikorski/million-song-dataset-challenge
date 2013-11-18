42a109eac8168c7b987e14074c06e8eab5c2d9ef
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-11-18 13:38:02 +0100

    Jaccard based knn for users

---
+++ create_plays_by_user_simple.py
@@ -0,0 +1,51 @@
+from pymongo import MongoClient, DESCENDING
+from utils.database_inserter import insert_batch
+from utils.measurements import invoke_measurable_task
+
+MONGODB_PORT = 27017
+NUMBER_OF_MOST_POPULAR_SONGS = 100
+BATCH_SIZE = 2500
+
+
+def create_plays_by_user_binary(plays_by_user, most_popular_songs, target_collection):
+    plays_batch = []
+
+    for user_play_map in plays_by_user.find():
+        entry = {'_id': user_play_map['_id'], 'value': []}
+        for song in most_popular_songs:
+            song_index = str(int(song['_id']))
+            if song_index in user_play_map['value']:
+                entry['value'].append(int(song_index))
+
+        plays_batch.append(entry)
+
+        if len(plays_batch) % BATCH_SIZE == 0:
+            insert_batch(plays_batch, target_collection)
+            plays_batch = []
+
+    if len(plays_batch) > 0:
+        insert_batch(plays_batch, target_collection)
+
+
+with MongoClient('localhost', MONGODB_PORT) as client:
+    db = client.local
+
+    most_popular_songs = [1]
+
+    def load_most_popular_songs():
+        most_popular_songs[0] = list(
+            db.play_count_by_song_t.find()
+            .sort('value', DESCENDING)
+            .limit(NUMBER_OF_MOST_POPULAR_SONGS)
+        )
+
+    invoke_measurable_task(load_most_popular_songs, 'Load %d most popular songs' % NUMBER_OF_MOST_POPULAR_SONGS)
+    most_popular_songs = most_popular_songs[0]
+
+    invoke_measurable_task(
+        lambda: create_plays_by_user_binary(db.plays_by_user_t, most_popular_songs, db.plays_by_user_simple_t),
+        "Create plays_by_user_simple collection for train set")
+
+    invoke_measurable_task(
+        lambda: create_plays_by_user_binary(db.plays_by_user_v, most_popular_songs, db.plays_by_user_simple_v),
+        "Create plays_by_user_simple collection for validation set")
---
+++ users_jaccard_based_knn.py
@@ -0,0 +1,59 @@
+from pymongo import MongoClient
+from utils.measurements import invoke_measurable_task
+
+MONGODB_PORT = 27017
+KNN_K = 50
+NUMBER_OF_USERS = 100
+
+
+class JaccardBasedKnn(object):
+    def __init__(self, plays_for_all_users, k):
+        self.plays_for_all_users = plays_for_all_users
+        self.k = k
+
+    def compute_jaccard_index(self, set_1, set_2):
+        if not set_1 and not set_2:
+            return None
+
+        n = len(set_1.intersection(set_2))
+        return n / float(len(set_1) + len(set_2) - n)
+
+    def find_for_user(self, user_plays):
+        scores = []
+
+        for other_user_plays in self.plays_for_all_users:
+            jaccard_index = self.compute_jaccard_index(user_plays, set(other_user_plays['value']))
+            scores.append({'user_id': other_user_plays['_id'], 'score': jaccard_index})
+
+        scores.sort(key=lambda x: x['score'], reverse=True)
+        scores = scores[0:self.k]
+        return scores
+
+    def find_for_users(self, users_plays):
+        scores = []
+
+        current_user_number = 1
+        for user_play in users_plays:
+            print 'Looking for knn for %s. user' % current_user_number
+            scores.append({'user_id': user_play['_id'], 'scores': self.find_for_user(set(user_play['value']))})
+            current_user_number += 1
+
+        return scores
+
+
+with MongoClient('localhost', MONGODB_PORT) as client:
+    db = client.local
+
+    plays_for_all_users = [1]
+    def load_plays_for_all_users(): plays_for_all_users[0] = list(db.plays_by_user_simple_t.find())
+    invoke_measurable_task(load_plays_for_all_users, 'Load plays for all users')
+    plays_for_all_users = plays_for_all_users[0]
+
+    plays_for_validated_users = [1]
+    def load_plays_for_all_users(): plays_for_validated_users[0] = list(db.plays_by_user_simple_v.find().limit(NUMBER_OF_USERS))
+    invoke_measurable_task(load_plays_for_all_users, 'Load plays for validated users')
+    plays_for_validated_users = plays_for_validated_users[0]
+
+    knn = JaccardBasedKnn(plays_for_all_users, KNN_K)
+    invoke_measurable_task(lambda: knn.find_for_users(plays_for_validated_users),
+                           'Find knn for %d users' % NUMBER_OF_USERS)
